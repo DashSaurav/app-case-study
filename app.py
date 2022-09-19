9c17756e659e878724bf0439cec0b9d455be7905
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2022-09-19 11:51:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,28 @@
-import pandas as pd
 import streamlit as st
-import hydralit_components as hc
+import datetime
+import calendar
+from home import home_ana
+from streamlit_option_menu import option_menu
+st.set_page_config(page_title='Retail App Analytics', layout='wide')
 
-df_log = pd.read_csv("login_logs.csv")
-df_s_or = pd.read_csv("sales_orders.csv")
-df_o_it = pd.read_csv("sales_orders_items.csv")
-st.sidebar.subheader('Plantix - Retailer App Case Study')
-df_log['seconds']=pd.to_timedelta(df_log['login_time']).dt.total_seconds()
+st.header("Case Study - Retail App Analytics")
+currenttime = datetime.datetime.today()
+onlydate = currenttime.day
+onlymonth = currenttime.strftime("%b")
+onlyyear = currenttime.year
+onlyday = calendar.day_name[currenttime.weekday()]
+st.sidebar.info(f"""{onlyday} , {onlydate} {onlymonth} {onlyyear}""")
 
-# total count of users
-tot_users = len(df_log['user_id'].unique())
-# how many users were there for amount of time.
-def min_users(sec):
-    data = df_log[df_log['seconds'] == sec]
-    data = len(data['user_id'])
-    return data
-# finding number of times user logged-in in the app.
-def user_log_time(ID):
-    data = df_log[df_log['user_id'] == ID]
-    data = len(data['login_time'])
-    return data
-# total stay time for an user
-def user_sty_time(ID):
-    data = df_log[df_log['user_id'] == ID]
-    data = sum(data['seconds'])
-    return data
+with st.sidebar:
+    sel = option_menu(
+        menu_title="Main Menu",
+        options=["Home"],
+        icons=["tv"],
+        menu_icon="cast",
+        #default_index=0,
+    )
 
-# hc.info_card(title='Application User Count', content=tot_users, sentiment='good')
-st.sidebar.metric(label='Total User Count', value=tot_users)
-st.sidebar.metric(label='Minimum App Used Time (seconds)', value=min(df_log['seconds']))
-st.sidebar.metric(label='User Count for Minimum Time', value=min_users(min(df_log['seconds'])))
-st.sidebar.metric(label='Maximum App Used Time (minutes)', value= round((max(df_log['seconds']))/60,2))
-st.sidebar.metric(label='User Count for Maximum Time', value=min_users(max(df_log['seconds'])))
+# sel = st.sidebar.radio("Select a Page",('Analysis', 'Predection'))
 
-cc = st.columns(3)
-with cc[0]:
-    st.header("Plantix - Case Study")
-cc = st.columns(3)
-with cc[0]:
-    sel_user = st.selectbox("Select a User", df_log['user_id'].unique())
-with cc[1]:
-    st.metric(label='Count of User Logged-In (Lifetime)', value=user_log_time(sel_user))
-with cc[2]:
-    st.metric(label='Total Stay time of User (in hours)', value=round(user_sty_time(sel_user)/3600,2))
-   
+if sel=='Home':
+    home_ana()
---
+++ home.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import streamlit as st
+
+df_log = pd.read_csv("login_logs.csv")
+df_s_or = pd.read_csv("sales_orders.csv")
+df_o_it = pd.read_csv("sales_orders_items.csv")
+df_log['seconds']=pd.to_timedelta(df_log['login_time']).dt.total_seconds()
+df_s_or['seconds']=pd.to_timedelta(df_s_or['creation_time']).dt.total_seconds()
+
+def home_ana():
+    # total count of users
+    tot_users = len(df_log['user_id'].unique())
+    # how many users were there for amount of time.
+    def min_users(sec):
+        data = df_log[df_log['seconds'] == sec]
+        data = len(data['user_id'])
+        return data
+    # finding number of times user logged-in in the app.
+    def user_log_time(ID):
+        data = df_log[df_log['user_id'] == ID]
+        data = len(data['login_time'])
+        return data
+    # total stay time for an user
+    def user_sty_time(ID):
+        data = df_log[df_log['user_id'] == ID]
+        data = sum(data['seconds'])
+        return data
+    
+    def sale_item(ID):
+        data = df_s_or[df_s_or['fk_buyer_id'] == ID]
+        data = sum(data['seconds'])
+        return data
+    # hc.info_card(title='Application User Count', content=tot_users, sentiment='good')
+    cc = st.columns(3)
+    with cc[0]:
+        st.metric(label='Total User Count', value=tot_users)
+    with cc[1]:
+        st.metric(label='Minimum App Used Time (seconds)', value=min(df_log['seconds']))
+    with cc[2]:
+        st.metric(label='User Count for Minimum Time', value=min_users(min(df_log['seconds'])))
+    cc = st.columns(3)
+    with cc[1]:
+        st.metric(label='Maximum App Used Time (minutes)', value= round((max(df_log['seconds']))/60,2))
+    with cc[2]:
+        st.metric(label='User Count for Maximum Time', value=min_users(max(df_log['seconds'])))
+
+    st.subheader("User Based Analytics")
+    cc = st.columns(3)
+    with cc[0]:
+        sel_user = st.selectbox("Select a User", df_log['user_id'].unique())
+    with cc[1]:
+        st.metric(label='Count of User Logged-In (Lifetime)', value=user_log_time(sel_user))
+    with cc[2]:
+        st.metric(label='Total Stay time of User (in hours)', value=round(user_sty_time(sel_user)/3600,2))
+    
+    cc = st.columns(3)
+    with cc[0]:
+        val = round(sale_item(sel_user)/3600,2)
+        if val == 0.0:
+            st.metric(label="Time used for ordering (in hours)", value= "Not visited")
+        else:
+            st.metric(label="Time used for ordering (in hours)", value= val)
+
+    st.subheader("Top 10 Products sold by our Application")
+    st.bar_chart(df_o_it['fk_product_id'].value_counts()[:10])
+
+    
